ac3146371b74a5711a907efe7c3575090eedef89
HCC7952889662 <hsucheng@usc.edu>
2020-10-08 00:09:48 +0800

    Add files via upload

---
+++ Circuit_Struct.py
@@ -0,0 +1,77 @@
+class Node:
+	def __init__(self, name: str, type:str):
+		self.name = name  # Node name
+		# Gate Type: ipt, not, nand, and, nor, or, xor, xnor, buff
+		self.gate_type = type  # Indicate which logic gate type this node is
+		self.fan_in_node = []  # The list of fan-in nodes
+		self.fan_out_node = []  # The list of fan-out nodes
+		self.value = None  # the output value of this gate
+
+	def __del__(self):
+		pass
+	def add_fan_in(self, x):
+		self.fan_in_node.append(x)
+
+	def add_fan_out(self, x):
+		self.fan_out_node.append(x)
+
+	def remove_fan_in(self, x):
+		self.fan_in_node.remove(x)
+
+	def remove_fan_out(self, x):
+		self.fan_out_node.remove(x)
+
+
+class Ckt:
+	def __init__(self):
+		self.circuit_name = None  # The circuit name
+		self.node_list = []  # The list storing all nodes
+		self.node_name_list = [] # Name Information
+		self.PI = []  # Primary input
+		self.PO = []  # Primary output
+		self.KI = []  # Key input
+		self.PI_count = 0 # IPT numbers
+		self.PO_count = 0 #PO numbers
+		self.node_count = 0 # How many nodes in the circuit
+
+	def __del__(self):
+		pass
+
+	def add_object(self, obj):
+		self.node_list.append(obj)
+		self.node_name_list.append(obj.name)
+		self.node_count += 1
+
+
+	def add_PI(self, obj):
+		self.add_object(obj)
+		self.PI_count += 1
+
+	def add_PO(self, obj):  # Add an object to the PO list
+		self.add_object(obj)
+		self.PO_count += 1
+
+
+	def remove_node_from_PI(self, obj):
+		self.PI.remove(obj)
+
+	def remove_node_from_PO(self, obj):
+		self.PO.remove(obj)
+
+	def pc(self):
+		for obj in self.node_list:
+			print(obj.name)
+			print('fan_in:', end= ' ')
+			for fi in obj.fan_in_node:
+				print(fi.name, end= ' ')
+			print('\nfan_out:', end= ' ')
+			for fo in obj.fan_out_node:
+				print(fo.name, end= ' ')
+			print('\n')
+
+class connect():
+	def __init__(self,type, name):
+		self.type = type ##{'wire':1, 'reg':2}
+		self.name = name
+		self.input_node  = [] ## this wire is the Input of nodes in this list
+		self.output_node = [] ## this wire is the Output of nodes in this list
---
+++ Parser.py
@@ -0,0 +1,92 @@
+# Executed in Python 3.6
+import re
+from Circuit_Struct import *
+
+def verilog_parser(filename):
+    Circuit = Ckt()
+    ipt = open(filename)
+    connection_info = []
+    eff_line = ''
+
+    for line in ipt:
+        # eliminate comment first
+        line_syntax = re.match(r'^.*//.*', line, re.IGNORECASE)
+        if line_syntax:
+            line = line[:line.index('//')]
+
+        # considering ';' issues
+        if ';' not in line and 'endmodule' not in line:
+            eff_line = eff_line + line.rstrip()
+            continue
+        line = eff_line + line.rstrip()
+        eff_line = ''
+        if line != "":
+            # wire
+            line_syntax = re.match(r'^[\s]*wire (.*,*);', line, re.IGNORECASE)
+            if line_syntax:
+                for n in line_syntax.group(1).replace(' ', '').replace('\t', '').split(','):
+                    new_connect = connect('wire', n)
+                    connection_info.append(new_connect)
+
+            # PI
+            line_syntax = re.match(r'^.*input ([a-z]+\s)*(.*,*).*;', line, re.IGNORECASE)
+            if line_syntax:
+                for n in line_syntax.group(2).replace(' ', '').replace('\t', '').split(','):
+                    new_node = Node(n, 'ipt')
+                    Circuit.add_PI(new_node)
+                    new_connect = connect('ipt', n)
+                    new_connect.input_node.append(new_node)
+                    connection_info.append(new_connect)
+
+            # PO
+            line_syntax = re.match(r'^.*output ([a-z]+\s)*(.*,*).*;', line, re.IGNORECASE)
+            if line_syntax:
+                for n in line_syntax.group(2).replace(' ', '').replace('\t', '').split(','):
+                    new_node = Node(n, 'opt')
+                    Circuit.add_PO(new_node)
+                    new_connect = connect('opt', n)
+                    new_connect.output_node.append(new_node)
+                    connection_info.append(new_connect)
+
+            # Module or Gate
+            line_syntax = re.match(r'\s*(.+?) (.+?)\s*\((.*)\s*\);$', line, re.IGNORECASE)
+            if line_syntax:
+                if line_syntax.group(1) == 'module':
+                    Ckt.name = line_syntax.group(2)
+
+                else:
+                    gate_order = line_syntax.group(3).replace(' ', '').split(',')
+                    new_node = Node(line_syntax.group(2), line_syntax.group(1))
+                    Circuit.add_object(new_node)
+                    #Default Output is 1, so the gate order is OIIIIIII...
+                    for index in range(len(gate_order)):
+                        for C in connection_info:
+                            # Output
+                            if index == 0:
+                                if C.name == gate_order[index]:
+                                    C.input_node.append(new_node)
+                            # Input
+                            else:
+                                if C.name == gate_order[index]:
+                                    C.output_node.append(new_node)
+    ipt.close()
+
+    # Dealing with the connection
+    for c in connection_info:
+        for i in c.input_node:
+            for o in c.output_node:
+                o.fan_in_node.append(i)
+                i.fan_out_node.append(o)
+
+
+    return Circuit
+
+
+try:
+    #circuit_lock('s298')
+    ckt = verilog_parser('ckt/c17.v')
+    ckt.pc()
+
+
+except IOError:
+    print("error in the code")
